f5acb6dec0a75435443958e68012e9db0b4fe7f7
Parth Patel <parthpatel6037@gmail.com>
2023-12-05 22:34:45 +0530

    reformatted using black

--- KAMAxoCrossingUp.py
+++ KAMAxoCrossingUp.py
@@ -1,4 +1,4 @@
-import math,csv, random
+import math, csv, random
 import datetime as dt
 import numpy as np
 import pandas as pd
@@ -17,31 +17,40 @@ df1, df2, df3, dfV = None, None, None, None
 
 # Read data
 
+
 def ReadData():
     global df1
-    df1 = pd.read_csv(file_name_base, delimiter=',', index_col='Date', parse_dates=True)
+    df1 = pd.read_csv(file_name_base, delimiter=",", index_col="Date", parse_dates=True)
     df1 = df1[df1.index >= start]
     df1 = df1[df1.index <= stop]
 
     if file_name_market2 != None and len(file_name_market2) > 0:
         global df2
-        df2 = pd.read_csv(file_name_market2, delimiter=',', index_col='Date', parse_dates=True)
+        df2 = pd.read_csv(
+            file_name_market2, delimiter=",", index_col="Date", parse_dates=True
+        )
         df2 = df2[df2.index >= start]
         df2 = df2[df2.index <= stop]
 
     if file_name_market3 != None and len(file_name_market3) > 0:
         global df3
-        df3 = pd.read_csv(file_name_market3, delimiter=',', index_col='Date', parse_dates=True)
+        df3 = pd.read_csv(
+            file_name_market3, delimiter=",", index_col="Date", parse_dates=True
+        )
         df3 = df3[df3.index >= start]
         df3 = df3[df3.index <= stop]
 
     if file_name_Vix != None and len(file_name_Vix) > 0:
         global dfV
-        dfV = pd.read_csv(file_name_Vix, delimiter=',', index_col='Date', parse_dates=True)
+        dfV = pd.read_csv(
+            file_name_Vix, delimiter=",", index_col="Date", parse_dates=True
+        )
         dfV = dfV[dfV.index >= start]
         dfV = dfV[dfV.index <= stop]
 
-#CUSTOM INDICATOR FUNCTION (main entry point called from TradersToolbox)
+
+# CUSTOM INDICATOR FUNCTION (main entry point called from TradersToolbox)
+
 
 def GetCustomSignal():
     ReadData()
@@ -49,10 +58,15 @@ def GetCustomSignal():
 
     global df1
     window, pow1, pow2 = 20, 2, 30
-    df1['KAMA'] = talib.KAMA(df1['Close'].values, window)
-    Signal = [1 if df1['Close'][i] >= df1['KAMA'][i] and df1['Close'][i - 1] < df1['KAMA'][i - 1] else 0 for i, m in
-              enumerate(df1['KAMA'])]
+    df1["KAMA"] = talib.KAMA(df1["Close"].values, window)
+    Signal = [
+        1
+        if df1["Close"][i] >= df1["KAMA"][i]
+        and df1["Close"][i - 1] < df1["KAMA"][i - 1]
+        else 0
+        for i, m in enumerate(df1["KAMA"])
+    ]
     df1["Signal"] = Signal
 
-    #Should return list of int (same length as Close)
+    # Should return list of int (same length as Close)
     return Signal
